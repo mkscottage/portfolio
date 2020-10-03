33888bfc512ded28b7d12d076a293263a8872b76
Mukesh Sharma <mkscottage@gmail.com>
2020-10-03 22:44:57 +0530

    Updates

---
+++ precache-manifest.d1bd329834088eb844b85c81d275fa9c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f984762d7452775686dbe0fca6014838",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "952be689db7533f26cf6",
+    "url": "/portfolio/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "d12871adcd8efcc910f8",
+    "url": "/portfolio/static/css/main.2395ba0c.chunk.css"
+  },
+  {
+    "revision": "952be689db7533f26cf6",
+    "url": "/portfolio/static/js/2.a5649bf6.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/portfolio/static/js/2.a5649bf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d12871adcd8efcc910f8",
+    "url": "/portfolio/static/js/main.c141d558.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "88073f981fde85b9cf3fa5a3a642f786",
+    "url": "/portfolio/static/media/bootstrap-icons.88073f98.svg"
+  },
+  {
+    "revision": "e6e486be9732dc8ffe2d85144c502650",
+    "url": "/portfolio/static/media/my_pic.e6e486be.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.bb2971971c7e8dbb6f1136fc97b1e3ff.js"
+  "/portfolio/precache-manifest.d1bd329834088eb844b85c81d275fa9c.js"
 );
 
 self.addEventListener('message', (event) => {
